56fd809e8dfed9ec52b0b99fd6f349fc99af3543
sanjusaji94 <sanjusaji94@gmail.com>
2025-06-29 21:33:06 +0200

    Univariate tab

--- app.R
+++ app.R
@@ -19,13 +19,15 @@ world_map$ISO3 <- countrycode(world_map$region, origin = "country.name", destina
 # Join map and data
 data_map <- left_join(world_map, data, by = "ISO3", relationship = "many-to-many")
 
-
+# ui
 ui <- fluidPage(
   titlePanel("World Data Explorer"),
   
   p("This app allows you to explore global indicators from the CIA World Factbook (2020)."),
   
   tabsetPanel(
+    
+    # Tab 1: Univariate Analysis
     tabPanel("Univariate Analysis",
              sidebarLayout(
                sidebarPanel(
@@ -56,8 +58,39 @@ ui <- fluidPage(
                  )
                )
              )
+    ),
+    
+    # Tab 2: Multivariate Analysis
+    tabPanel("Multivariate Analysis",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("x_var", "X-axis Variable:",
+                             choices = c(
+                               "Education Expenditure" = "expenditure",
+                               "Youth Unemployment Rate" = "youth_unempl_rate",
+                               "Net Migration Rate" = "net_migr_rate",
+                               "Population Growth Rate" = "pop_growth_rate",
+                               "Electricity from Fossil Fuels" = "electricity_fossil_fuel",
+                               "Life Expectancy" = "life_expectancy"
+                             )),
+                 selectInput("y_var", "Y-axis Variable:",
+                             choices = c(
+                               "Education Expenditure" = "expenditure",
+                               "Youth Unemployment Rate" = "youth_unempl_rate",
+                               "Net Migration Rate" = "net_migr_rate",
+                               "Population Growth Rate" = "pop_growth_rate",
+                               "Electricity from Fossil Fuels" = "electricity_fossil_fuel",
+                               "Life Expectancy" = "life_expectancy"
+                             )),
+                 selectInput("size_var", "Size by:",
+                             choices = c("Population" = "population", "Area" = "area"))
+               ),
+               mainPanel(
+                 plotlyOutput("scatter_plot")
+               )
+             )
     )
-  
+    
   )
 )
 
@@ -96,6 +129,81 @@ server <- function(input, output, session) {
     
     ggplotly(p, tooltip = "text")
   })
+  
+  # Boxplot (global)
+
+  output$boxplot_global <- renderPlotly({
+    var <- selected_var()
+    
+    p <- ggplot(data %>% filter(!is.na(.data[[var]])),
+                aes(y = .data[[var]])) +
+      geom_boxplot(fill = "steelblue", color = "black") +
+      labs(
+        y = var_labels[[var]],
+        title = paste("Boxplot of", var_labels[[var]])
+      ) +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  
+  # Histogram + Density (global)
+  
+  output$histogram_global <- renderPlotly({
+    var <- selected_var()
+    
+    p <- ggplot(data %>% filter(!is.na(.data[[var]])), aes(x = .data[[var]])) +
+      geom_histogram(aes(y = after_stat(density)), bins = 30,
+                     fill = "skyblue", color = "black", alpha = 0.6) +
+      geom_density(color = "darkblue", size = 1.2) +
+      labs(
+        x = var_labels[[var]],
+        title = paste("Histogram & Density of", var_labels[[var]])
+      ) +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+ # Boxplot by continent
+  
+  output$boxplot_continent <- renderPlotly({
+    var <- selected_var()
+    
+    p <- ggplot(data %>% filter(!is.na(.data[[var]])),
+                aes(x = continent, y = .data[[var]], fill = continent)) +
+      geom_boxplot() +
+      labs(
+        x = "Continent",
+        y = var_labels[[var]],
+        title = paste("Boxplot of", var_labels[[var]], "by Continent")
+      ) +
+      scale_fill_brewer(palette = "Set2")+
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  #Grouped density plot by continent
+  
+  output$density_continent <- renderPlotly({
+    var <- selected_var()
+    
+    p <- ggplot(data %>% filter(!is.na(.data[[var]])),
+                aes(x = .data[[var]], fill = continent, color = continent)) +
+      geom_density(alpha = 0.4) +
+      labs(
+        x = var_labels[[var]],
+        title = paste("Density Plot of", var_labels[[var]], "by Continent")
+      ) +
+      scale_fill_viridis_d(option = "D") +
+      scale_color_viridis_d(option = "D") +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
 }
 
 # --- Run the App ---
